2e6e1d6b604d8ed37e6cda77ec979460c0e866fa
peacefum <titaum@vivianproj.com>
2021-07-21 13:14:22 +0900

    머테리얼 클릭하는애

---
+++ DrawMatClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawMatClick : MonoBehaviour
+{
+    DrawLine d;
+    Button b;
+    private void Start()
+    {
+        b = GetComponent<Button>();
+
+        d = FindObjectOfType<DrawLine>();
+        b.onClick.AddListener(() => { MatClick(); });
+    }
+    public void MatClick()
+    {
+      d._currentLineMat = 
+            transform.GetComponent<Image>().material;
+    }
+}
