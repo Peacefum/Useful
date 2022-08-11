0604d8eec0a513d6f26faa1c9a87c614f4d0f6ab
peacefum <titaum@vivianproj.com>
2022-08-11 15:20:58 +0900

    mesh관련

---
+++ CombinedMesh.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class CombinedMesh : MonoBehaviour
+{
+    [SerializeField] MeshFilter[] meshFilters;
+    [SerializeField] Material mat;
+
+    void OnEnable() {
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+
+            i++;
+        }
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        transform.GetComponent<Renderer>().material = mat;
+        transform.gameObject.SetActive(true);
+    }
+}
